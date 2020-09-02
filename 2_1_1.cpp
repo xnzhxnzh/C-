9cf32332dad82944688bb0d06d95d7df93e7056a
xnzhxnzh <35820138+xnzhxnzh@users.noreply.github.com>
2020-09-01 22:12:26 -0700

    Add files via upload

---
+++ 2_1_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  
+  std::cin >> tip;
+  
+  std::cout<<"You paid "<<tip<<" dollors"<<"\n";
+
+}
