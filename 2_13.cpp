54db56e6298f5f55406dd4b6d7f9341831241d91
xnzhxnzh <35820138+xnzhxnzh@users.noreply.github.com>
2020-09-19 23:14:09 -0700

    Add files via upload

---
+++ 2_13.cpp
@@ -0,0 +1,19 @@
+//Make a program:
+//Display "Please input days:" to ask user to input a number of days(example: 3)
+//Convert it into minutes(example: 3 days =3*24*60=4320 minutes)
+//Print the result as "The [days] days have [minutes] minutes."
+//Please fill in the blank to make the program completed.
+
+#include <iostream>
+int main() 
+{
+  int days = 0;
+  std::cout << "Please input days: ";
+  std::cin >> days;
+  int minutes=__________;
+  std::cout<<"The "<<days<<" days have "<<minutes<<" minutes.\n";
+
+}
+
+
+
